5927f7691ae357cb94f466b9dc28d6237cbf0b88
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-03 13:50:56 +0600

    solved team problem

--- solution.cpp
+++ solution.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-bool check_number(string str) {
-   for (int i = 0; i < str.length(); i++) {
-      if (!isdigit(str[i]))
-         return false;
-   }
-   return true;
-}
+int main()
+{
+   int n, Petya, Vasya, Tonya, number(0);
 
-int main() {
-   std::string word;
-   std::string result;
+   cin >> n;
 
-   std::cin >> word;
-
-   if (!check_number(word)) {
-      if (word.size() > 10) {
-         result = word[0] + to_string(word.size() - 2) + word.back();
-         std::cout << result;
-      } else {
-         std::cout << word;
+   while (n--)
+   {
+      cin >> Petya >> Vasya >> Tonya;
+      if (Petya + Vasya + Tonya >= 2)
+      {
+         number += 1;
       }
    }
+
+   cout << number << endl;
 
    return 0;
 }
