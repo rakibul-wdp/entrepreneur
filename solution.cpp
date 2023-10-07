f0f2ba765837ebb6c359ac0f99dff138b0ee1a29
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-07 12:58:15 +0600

    solved beautiful matrix problem

--- solution.cpp
+++ solution.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-   int w;
+   int a[5][5];
 
-   cin >> w;
-
-   if (w % 2 == 0 && w > 2)
-      cout << "Yes";
-   else
-      cout << "No";
+   for (int i = 0; i < 5; i++)
+   {
+      for (int j = 0; j < 5; j++)
+      {
+         cin >> a[i][j];
+         if (a[i][j] == 1)
+         {
+            cout << abs(2 - i) + abs(2 - j);
+         }
+      }
+   }
 
    return 0;
 }
