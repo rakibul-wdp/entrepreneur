1c9220c06b986ebbfdc46119b085351f54817344
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-10 10:59:55 +0600

    solved ali baba and puzzles

--- D_Ali_Baba_and_Puzzles.cpp
+++ D_Ali_Baba_and_Puzzles.cpp
@@ -3,14 +3,20 @@ using namespace std;
 
 int main()
 {
-  int a, b, c, d;
+  long long a, b, c, d;
   cin >> a >> b >> c >> d;
 
-  for (int i = 1; i <= 3; i++)
-  {
-  }
-
   if (a + b * c == d)
+    cout << "YES" << endl;
+  else if (a + b - c == d)
+    cout << "YES" << endl;
+  else if (a - b + c == d)
+    cout << "YES" << endl;
+  else if (a - b * c == d)
+    cout << "YES" << endl;
+  else if (a * b + c == d)
+    cout << "YES" << endl;
+  else if (a * b - c == d)
     cout << "YES" << endl;
   else
     cout << "NO" << endl;
