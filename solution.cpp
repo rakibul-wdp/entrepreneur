1d95b5d4842b9ce480d6358d27c5b6228db0467f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-29 03:39:00 +0600

    solved welcome for you with conditions

--- solution.cpp
+++ solution.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-   int a[5][5];
+   int A, B;
 
-   for (int i = 0; i < 5; i++)
-   {
-      for (int j = 0; j < 5; j++)
-      {
-         cin >> a[i][j];
-         if (a[i][j] == 1)
-         {
-            cout << abs(2 - i) + abs(2 - j);
-         }
-      }
-   }
+   cin >> A >> B;
+   A >= B ? cout << "Yes" : cout << "No";
 
    return 0;
 }
