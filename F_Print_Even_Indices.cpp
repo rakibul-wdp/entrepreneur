03a5247fd6cd2e24ad6ca30a02f2f41ca3ccf6d6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-04 18:10:55 +0600

    print even indices

--- F_Print_Even_Indices.cpp
+++ F_Print_Even_Indices.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long sum = 0;
 void fun(int arr[], int n, int i)
 {
   if (i == n)
     return;
 
+  sum += arr[i];
+
   fun(arr, n, i + 1);
-  if (i % 2 == 0)
-    cout << arr[i] << " ";
 }
 
 int main()
@@ -20,6 +21,7 @@ int main()
     cin >> arr[i];
 
   fun(arr, n, 0);
+  cout << sum;
 
   return 0;
 }
