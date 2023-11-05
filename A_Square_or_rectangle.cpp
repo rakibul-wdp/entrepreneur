69620423f6a47fbf2ce9d9f3e39c0f388fe58ea5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-05 13:16:04 +0600

    square or rectangle

---
+++ A_Square_or_rectangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+
+    if (a == b)
+      cout << "Square" << endl;
+    else
+      cout << "Rectangle" << endl;
+  }
+
+  return 0;
+}
