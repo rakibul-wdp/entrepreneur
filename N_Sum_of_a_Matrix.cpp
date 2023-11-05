ad5fe01566e9485c39a5d00837b330ce1ba949e3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-05 09:52:02 +0600

    sum of matrix

---
+++ N_Sum_of_a_Matrix.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int row, col;
+  cin >> row >> col;
+  int arr[row][col], arr2[row][col];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+      cin >> arr[i][j];
+  }
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+      cin >> arr2[i][j];
+  }
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      cout << arr[i][j] + arr2[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
