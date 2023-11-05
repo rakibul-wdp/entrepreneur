1c78351ec82f4ade62d903b8dbc86c22a5cc1930
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-06 01:39:00 +0600

    solved even numbers problem

--- solution.cpp
+++ solution.cpp
@@ -3,19 +3,17 @@ using namespace std;
 
 int main()
 {
-   int a, b;
-   char s;
+   int n;
 
-   cin >> a >> s >> b;
+   cin >> n;
 
-   if (s == '+')
-      cout << a + b;
-   else if (s == '-')
-      cout << a - b;
-   else if (s == '*')
-      cout << a * b;
-   else
-      cout << a / b;
+   for (int i = 1; i <= n; i++)
+   {
+      if (i % 2 == 0)
+         cout << i << "\n";
+   }
+   if (n == 1)
+      cout << -1;
 
    return 0;
 }
