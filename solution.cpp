f5bfd4cfc1d33a99e071451a6dbe66d18ca4cca2
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-07 01:39:00 +0600

    solved digits space problem

--- solution.cpp
+++ solution.cpp
@@ -9,7 +9,15 @@ int main()
 
    for (int i = 1; i <= n; i++)
    {
-      cout << n << " * " << i << " = " << n * i << endl;
+      string number;
+      cin >> number;
+      int size = number.size();
+
+      for (int z = size - 1; z >= 0; z--)
+      {
+         cout << number[z] << " ";
+      }
+      cout << endl;
    }
 
    return 0;
