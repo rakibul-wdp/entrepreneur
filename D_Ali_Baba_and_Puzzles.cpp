8a19650f291b035fa2b1c70dce34132caf5a3bd8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-12 03:39:00 +0600

    solved divisor problem

---
+++ D_Ali_Baba_and_Puzzles.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  for (int i = 1; i <= 3; i++)
+  {
+  }
+
+  if (a + b * c == d)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+
+  return 0;
+}
