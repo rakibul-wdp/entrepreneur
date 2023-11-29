b463db05c6ab10e6a84ce9ac909985b5f0f7f5a1
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-30 02:39:00 +0600

    create a new string

---
+++ A_Create_A_New_String.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char s[1000], t[1000];
+  cin >> s >> t;
+
+  cout << strlen(s) << " " << strlen(t) << endl;
+
+  cout << s << " " << t << endl;
+
+  return 0;
+}
