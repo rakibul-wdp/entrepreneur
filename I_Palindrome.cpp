c567e8afc65d30ba482871fcfa6408d4eacdd707
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-24 10:01:38 +0600

    I Palindrome

--- I_Palindrome.cpp
+++ I_Palindrome.cpp
@@ -3,60 +3,33 @@ using namespace std;
 
 int main()
 {
-  string s, t;
-  cin >> s;
+  int n;
+  cin >> n;
 
-  t = s;
+  int reversed = 0;
+  int original = n;
 
-  int i = 0,
-      j = s.size() - 1;
-  while (i < j)
+  while (n != 0)
   {
-    char tmp = s[i];
-    s[i] = s[j];
-    s[j] = tmp;
-    i++;
-    j--;
+    int digit = n % 10;
+    n = n / 10;
+
+    if (reversed != 0 || digit != 0)
+    {
+      reversed = reversed * 10 + digit;
+    }
   }
 
-  int flag = 0;
-  for (int i = 0; i < s.size(); i++)
-  {
-    for (int j = 0; j < t.size(); j++)
-      if (s[j] == t[i])
-        flag = 1;
-      else
-        flag = 0;
-  }
+  cout << reversed << endl;
 
-  if (flag)
-    cout << s << endl
-         << "YES" << endl;
+  if (original == reversed)
+  {
+    cout << "YES" << endl;
+  }
   else
   {
-    int flag2 = 0;
-    for (int i = 0; i < s.size(); i++)
-    {
-      if (s[0] == '0')
-        flag2 = 1;
-    }
-    if (flag2)
-      cout << s.erase(0, 1) << endl
-           << "NO" << endl;
-    else
-      cout << s << endl
-           << "NO" << endl;
+    cout << "NO" << endl;
   }
 
   return 0;
 }
-
-/*
-
-000250
-052000
-
-52000
-NO
-
-*/
