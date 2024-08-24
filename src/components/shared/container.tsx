9fd597c29eb0308b40f2db2ae5abd170afcf1380
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-25 03:39:00 +0600

    create container component

---
+++ src/components/shared/container.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from "react";
+
+type TContainerProps = HTMLAttributes<HTMLDivElement>;
+
+const Container = ({ className, children, ...props }: TContainerProps) => {
+  return (
+    <div className={`px-4 2xl:px-0 max-w-7xl mx-auto ${className}`} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
