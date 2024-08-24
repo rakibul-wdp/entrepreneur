787582ee2e3be1b4dc2515fbddd5a81ed2dc2a9f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-25 02:39:00 +0600

    create sign in page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "@/components/auth/auth-form";
+
+const page = () => {
+  return (
+    <div className="bg-[#024044] h-screen flex justify-center items-center">
+      <AuthForm />
+    </div>
+  );
+};
+
+export default page;
