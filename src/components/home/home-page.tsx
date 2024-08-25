e3f7af85e0e8af7269a4e441027529354bae7f75
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-25 07:39:00 +0600

    create home page

---
+++ src/components/home/home-page.tsx
@@ -0,0 +1,13 @@
+import Features from "./features";
+import HeroSection from "./hero-section";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <Features />
+    </>
+  );
+};
+
+export default HomePage;
