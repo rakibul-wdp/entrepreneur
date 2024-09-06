8a19b0258c8616e6c20570c12307a8c912775292
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-06 15:17:34 +0600

    create nextjs project

--- utils/calculateprice.ts
+++
@@ -1,8 +0,0 @@
-export const calculatePrice = (
-  calculable: any,
-  getProperty: (item: any) => number
-) => {
-  return calculable.reduce((total: number, item: any) => {
-    return total + getProperty(item);
-  }, 0);
-};
